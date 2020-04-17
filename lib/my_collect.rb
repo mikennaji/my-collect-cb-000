1d0245715c2beaa2a17dbf7d934589bace35ac3c
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-17 17:34:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 def  my_collect(array)
   i=0
   collection = []
-  if block_given?
+  
     while i < array.size
       collection << yield(array[i])
       i += 1
     end
-  end
+
   return collection
 
 end
